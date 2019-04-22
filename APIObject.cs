abd59f23f120691e05693f7f6572e2d557163bce
kulemin <kulemin86@bk.ru>
2019-04-22 23:16:42 +0400

    First commit

---
+++ APIObject.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memory.API
+{
+    public class APIObject : IDisposable
+    {
+        int value;
+        bool isDisposed;
+        public APIObject(int value)
+        {
+            this.value = value;
+            MagicAPI.Allocate(value);
+        }
+
+        ~APIObject()
+        {
+            Dispose(value);
+        }
+
+        public void Dispose()
+        {
+            if (!isDisposed)
+                MagicAPI.Free(value);
+            GC.SuppressFinalize(this);
+            isDisposed = true;
+        }
+
+        public void Dispose(int value)
+        {
+            if (!isDisposed)
+                MagicAPI.Free(value);
+            isDisposed = true;
+        }
+    }
+}
+
+
